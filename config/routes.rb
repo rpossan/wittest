10c7ea6d7bc24fc8e46ece5f2143fa5ae36b5485
Ronaldo Possan <ronaldo.possan@thomsonreuters.com>
2017-11-12 23:01:37 -0200

    Adds models for User, Product, Cart and CartItem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cart_items
+  resources :carts
+  resources :users
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
